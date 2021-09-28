b07146bdd2aa2584222a9dc587a5de55622cb6c3
tkreind <tkreind@umich.edu>
2021-09-28 12:25:34 -0400

    Added event capture to content.js

--- src/content.ts
+++ src/content.ts
@@ -1,2 +1,14 @@
 console.log('Hello Content');
 export {}
+
+const handleClicks = (e: MouseEvent) => {
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+    console.log(e.target);
+}
+
+window.onclick = handleClicks
