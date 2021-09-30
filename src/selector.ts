0ecab6e83f56e8410a6c43767c3931a9755dae45
tkreind <tkreind@umich.edu>
2021-09-30 14:40:50 -0400

    Got mouse hovering working

--- src/selector.ts
+++ src/selector.ts
@@ -9,7 +9,8 @@ const handleClicks = (e: MouseEvent) => {
         e.preventDefault();
     }
 
-    const target = e.target as Element;
+    const target = e.target as HTMLElement;
+    
     const rect = target.getBoundingClientRect();
     const center = {
         x: (rect.left + rect.right) / 2,
@@ -24,12 +25,50 @@ const handleClicks = (e: MouseEvent) => {
             data: center
         })
     }, (response) => {
-        console.log(response);
+        console.log("Response: " + response);
     });
 
     // Remove it:
     window.removeEventListener('click', handleClicks, false);
+
+    // and the highlighter
+    window.removeEventListener('mouseover', handleHover, false);
+    window.removeEventListener('mouseout', hoverEnd, false);
+
+    // remove the active highlighted color
+    target.style.backgroundColor = "";
+    target.style.color = "";
+}
+
+const handleHover = (e: MouseEvent) => {
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+
+    const target = e.target as HTMLElement;
+    
+    target.style.backgroundColor = "#f0a3ff";
+    target.style.color = "#000000";
+}
+
+const hoverEnd = (e: MouseEvent) => {
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+
+    const target = e.target as HTMLElement;
+    
+    target.style.backgroundColor = "";
+    target.style.color = "";
 }
 
 // Add the listener:
 window.addEventListener('click', handleClicks, false);
+window.addEventListener('mouseover', handleHover, false);
+window.addEventListener('mouseout', hoverEnd, false);
