3d6f590e27d8e94f1076e6f15657ba72367d1fcd
tkreind <tristankreindler@gmail.com>
2021-11-25 20:10:36 +0000

    add azure intent endpoint

--- src/other/Common/constants.ts
+++ src/other/Common/constants.ts
@@ -4,3 +4,6 @@ export const apiUri: string = "https://pigeon-ai-api.herokuapp.com"
 export const uploadControllerUri: string = apiUri + "/Upload"
 export const inferenceControllerUri: string = apiUri + "/Inference"
 export const summaryControllerUri: string = apiUri + "/Summary"
+
+// secret, get from Tristan to build
+export const azureIntentEndpoint: string = "SECRET, check heroku"
